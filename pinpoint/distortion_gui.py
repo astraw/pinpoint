6041db528f8e9e3a0dfca6c2ede48bff61d1be69
Andrew Straw <strawman@astraw.com>
2008-07-01 19:54:38 -0700

    multi-image viewing per parameter set now works

--- pinpoint/distortion_gui.py
+++ pinpoint/distortion_gui.py
@@ -2,7 +2,7 @@
 from threading import Thread
 from enthought.pyface.api import SplitApplicationWindow, GUI
 
-from enthought.traits.api import HasTraits, Float, Instance, String, File, Button, Array, Delegate
+from enthought.traits.api import HasTraits, Float, Instance, String, File, Button, Array
 import enthought.traits.api as traits
 from enthought.traits.ui.api import View, Item, Group
 
@@ -19,20 +19,14 @@ from distortion import NonlinearDistortionParameters, CaltechNonlinearDistortion
 
 class RightSidePanel(HasTraits):
     # right or bottom panel depending on direction of split
-    button = Button
+    button = Button # add new filename
     distortion_instance = Instance(NonlinearDistortionParameters)
 
     traits_view = View( 'button',
                         'distortion_instance')
-    ## def __init__(self,*args,**kwargs):
-    ##     super(RightSidePanel,self).__init__(*args,**kwargs)
-    ##     self.distortion_instance.on_trait_change(self.xxx)
-    ## def xxx(self,event):
-    ##     print 'traits changed'
 
 class DistortionWorkThread(Thread):
     def run(self):
-        print 'running'
         im,ll,ur = self.distortion_instance.remove_distortion(self.image_data)
 
         self.mplwidget.axes.images=[]
@@ -43,53 +37,30 @@ class DistortionWorkThread(Thread):
         GUI.invoke_later(self.mplwidget.figure.canvas.draw)
 
 class DistortedImageWidget(Widget):
-    #distortion_instance = Instance(NonlinearDistortionParameters)
-    #distortion_instance = Delegate('param_holder')
+    distortion_instance = Instance(NonlinearDistortionParameters)
     param_holder = Instance(RightSidePanel)
     list_of_lines = traits.List
     mplwidget = Instance(MPLWidget)
     distorted_image = Array
     processing_job = Instance(DistortionWorkThread)
 
-    def __init__(self, parent, distorted_image, param_holder, **kwargs):
+    def __init__(self, parent, distorted_image, distortion_instance, **kwargs):
         self.distorted_image = distorted_image
-        self.param_holder = param_holder
+        self.distortion_instance = distortion_instance
         super(DistortedImageWidget,self).__init__(**kwargs)
-
-        if 0:
-            self.mplwidget = MPLWidget(self._panel)
-            self.control = self.mplwidget.control
-        else:
-            self.control = self._create_control(parent)
-
-        self.param_holder.distortion_instance.on_trait_change(self._show_undistorted_image)
-        print 'registered show() for',self.param_holder.distortion_instance
+        self.control = self._create_control(parent)
+        self.distortion_instance.on_trait_change(self._show_undistorted_image)
+        self._show_undistorted_image()
 
     def _create_control(self, parent):
         """ Create the toolkit-specific control that represents the widget. """
         # The panel lets us add additional controls.
         self._panel = wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
-        if 0:
-            self._sizer = wx.BoxSizer(wx.VERTICAL)
-            print 'self._sizer',self._sizer
-
-            self._panel.SetSizer(self._sizer)
-            print 'self._sizer',self._sizer
-        self.mplwidget = MPLWidget(self._panel) # why does this kill the sizer?
-        if 0:
-            print 'self._sizer',self._sizer
-            print 'self.mplwidget.control',self.mplwidget.control
-
-            self._sizer.Add(self.mplwidget.control, 1, wx.EXPAND)
-            print 'self._sizer',self._sizer
-            self._sizer.Layout()
-            print 'self._sizer',self._sizer
+        self.mplwidget = MPLWidget(self._panel)
         return self._panel
 
     def _show_undistorted_image(self):
-        print 'should start show()'
-        if self.param_holder.distortion_instance is None:
-            print 'no params! - no show'
+        if self.distortion_instance is None:
             return
         try:
             if self.processing_job.isAlive():
@@ -99,7 +70,7 @@ class DistortedImageWidget(Widget):
             pass
         self.processing_job = DistortionWorkThread()
         self.processing_job.mplwidget = self.mplwidget
-        self.processing_job.distortion_instance = self.param_holder.distortion_instance
+        self.processing_job.distortion_instance = self.distortion_instance
         self.processing_job.image_data = self.distorted_image
         self.processing_job.start()
 
@@ -112,7 +83,7 @@ class MainWindow(SplitApplicationWindow):
     _expandable = Instance(ExpandablePanel) # FIX1 = False
     #_expandable = Instance(wx.Panel) # FIX1 = True
 
-    panel = Instance(RightSidePanel)
+    rspanel = Instance(RightSidePanel)
     ratio = Float(0.8)
     title = 'pinpoint - camera distortion GUI'
     dis = traits.List
@@ -123,24 +94,15 @@ class MainWindow(SplitApplicationWindow):
         super(MainWindow,self).__init__(*args,**kwargs)
         self.dis = []
 
-    def xxx(self,event):
-        print 'traits changed'
-        for di in self.dis:
-            print di.param_holder.distortion_instance.r1
-
     def on_add_filename(self, filename):
         image_data = scipy.misc.pilutil.imread(filename)
         h,w = image_data.shape[:2]
         cc1 = w/2.0
         cc2 = h/2.0
 
-        if self.panel.distortion_instance is None:
+        if self.rspanel.distortion_instance is None:
             # the first time an image is loaded, the parameters are initialized
-            self.panel.distortion_instance = CaltechNonlinearDistortionParameters(cc1=cc1,cc2=cc2)
-            print 'created caltech params',self.panel.distortion_instance
-            self.panel.distortion_instance.on_trait_change(self.xxx)
-            print 'registered xxx() for',self.panel.distortion_instance
-
+            self.rspanel.distortion_instance = CaltechNonlinearDistortionParameters(cc1=cc1,cc2=cc2)
 
         parent = self._expandable.control
         if FIX1:
@@ -151,14 +113,14 @@ class MainWindow(SplitApplicationWindow):
             parent = panel
         di = DistortedImageWidget(parent,
                                   distorted_image=image_data,
-                                  #distortion_instance=self.panel.distortion_instance,
-                                  param_holder=self.panel)
+                                  distortion_instance=self.rspanel.distortion_instance,
+                                  )
         self.dis.append( di )
         if FIX1:
             pass
         else:
             self._expandable.add_panel(filename, di.control)
-            #self._expandable.show_layer(filename)
+            #self._expandable.show_layer(filename) # this is mentioned in expandable_panel.py source, but doesn't exist
 
     def _create_lhs(self, parent):
 
@@ -173,8 +135,8 @@ class MainWindow(SplitApplicationWindow):
         return self._expandable.control
 
     def _create_rhs(self, parent):
-        self.panel = RightSidePanel()
-        return self.panel.edit_traits(parent = parent, kind="subpanel").control
+        self.rspanel = RightSidePanel()
+        return self.rspanel.edit_traits(parent = parent, kind="subpanel").control
 
     def _on_close(self, event):
         self.close()
