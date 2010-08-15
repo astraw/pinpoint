7b24d39e76bc99467f6a9570aa2589481f6f0785
Andrew Straw <strawman@astraw.com>
2010-08-15 13:24:51 -0700

    add doc building script

---
+++ make-doc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+set -e
+
+git status > /dev/null && { echo "aborting - repo not clean. try git status"; exit 1; }
+
+rm -rf /tmp/gh-pages
+python setup.py build_sphinx
+cp -a docs/build/html /tmp/gh-pages
+
+git checkout gh-pages
+git clean -dxf
+git rm -r *
+cp -a /tmp/gh-pages/* .
+touch .nojekyll
+find . | xargs git add
+git commit -m "update pages"
+
+git checkout master
